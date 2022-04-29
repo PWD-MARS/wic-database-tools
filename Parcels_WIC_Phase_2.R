37e33d60c046e54d87f2ab7fef47d163a1871e11
Farshad Ebrahimi <Farshad.Ebrahimi@phila.gov>
2022-04-29 14:48:47 -0400

    DB comments addressed

--- Parcels_WIC_Phase_2.R
+++ Parcels_WIC_Phase_2.R
@@ -61,31 +61,31 @@
     ###Regex = in: a literal {, followed by a capture group containing 0 or more characters, followed by a literal }
     ###Regex = out: The first capture group
     
-    WORKORDER_ID$FACILITYID<-gsub("\\{(.*)\\}","\\1",as.character(WORKORDER_ID$FACILITYID))
+    WORKORDER_ID$facility_id<-gsub("\\{(.*)\\}","\\1",as.character(WORKORDER_ID$facility_id))
 
 
 ## Section 2: Matching by facility ID
     
   # Inner join the workorderid and Parcels_facility_id ON facility ID
     
-    WOID_FACID_Match <- inner_join (WORKORDER_ID, Parcels_Address_id  , by = "FACILITYID")
+    WOID_FACID_Match <- inner_join (WORKORDER_ID, Parcels_Address_id  , by = c("facility_id"="FACILITYID"))
    
    
   # Get FIRST OUTPUT BASED ON FACILITYID MATCH STORED IN WOID_FAC_UNIQ_REL
    
-    WOID_FAC_UNIQ_REL <- select(WOID_FACID_Match, WORKORDERID, LOCATION = ADDRESS, FACILITYID)
+    WOID_FAC_UNIQ_REL <- select(WOID_FACID_Match, workorder_id, location, facility_id)
   
   # Remove the rows that we matched based on the facilityid from the cityworks table 
      
-    WORKORDER_ID <- anti_join(WORKORDER_ID, WOID_FACID_Match, by = "WORKORDERID")
+    WORKORDER_ID <- anti_join(WORKORDER_ID, WOID_FACID_Match, by = "workorder_id")
     
   # Remove the rows that were matched from the Parcels_Address_id table
     
-    Parcels_Address_id <-anti_join(Parcels_Address_id, WOID_FACID_Match, by = "FACILITYID" )
+    Parcels_Address_id <-anti_join(Parcels_Address_id, WOID_FACID_Match, by = c("FACILITYID"="facility_id") )
     
   # drop the facility id from workorderid as these ids are not meaningful 
     
-    WORKORDER_ID_Location <-  select(WORKORDER_ID, -FACILITYID)
+    WORKORDER_ID_Location <-  select(WORKORDER_ID, -facility_id)
     
     
 ## Section 3: Matching by address
@@ -93,15 +93,15 @@
 
   # Inner join the  WORKORDER_ID_Location with Parcels_Address_id to extract the facility id
    
-    WO_ID_ADDRESS_MATCH <- inner_join(WORKORDER_ID_Location,Parcels_Address_id,by = c("LOCATION" = "ADDRESS"))
+    WO_ID_ADDRESS_MATCH <- inner_join(WORKORDER_ID_Location,Parcels_Address_id,by = c("location" = "ADDRESS"))
 
   # GET SECOND OUTPUT BASED ON FACILITYID MATCH STORED IN WOID_FAC_UNIQ_Add_REL )  
 
-    WOID_FAC_UNIQ_Add_REL <- WO_ID_ADDRESS_MATCH[c("WORKORDERID", "LOCATION", "FACILITYID")]
+    WOID_FAC_UNIQ_Add_REL <- WO_ID_ADDRESS_MATCH  %>% select(workorder_id, location, facility_id = FACILITYID)  
    
   # Remove the rows that we were matched based on the address from the cityworkstable 
    
-    WORKORDER_ID <- anti_join(WORKORDER_ID, WO_ID_ADDRESS_MATCH, by = "WORKORDERID")
+    WORKORDER_ID <- anti_join(WORKORDER_ID, WO_ID_ADDRESS_MATCH, by = "workorder_id")
     
   # Remove the rows that were matched from the Parcels_Address_id table
     
@@ -111,7 +111,7 @@
     
   # Build the spatial object from the XY coordinates in Cityworks
    
-    xy_coor <-  c(WORKORDER_ID[,"WOXCOORDINATE"],WORKORDER_ID[,"WOYCOORDINATE"])
+    xy_coor <-  c(WORKORDER_ID[,"wo_xcoordinate"],WORKORDER_ID[,"wo_ycoordinate"])
 
     mat_coor <- matrix(data = xy_coor, ncol = 2) %>% na.omit
   
@@ -162,7 +162,7 @@
      
      mat_coor <- data.frame (mat_coor)
      
-     names(mat_coor) <- c("WOXCOORDINATE","WOYCOORDINATE")
+     names(mat_coor) <- c("wo_xcoordinate","wo_ycoordinate")
      
      WO_MATCHED_XY <- mat_coor[Index_WO, ]
      
@@ -172,7 +172,7 @@
      
   # Get workorderid, address, and facilityid (Third output is stored in WOID_Based_XY )
      
-     WOID_Based_XY<- WORKORDER_ID %>% select(WORKORDERID, WO_INITIATEDATE, LOCATION, WOXCOORDINATE, WOYCOORDINATE) %>% inner_join(COORD_MATCHED_TABLE, by = c("WOXCOORDINATE" = "WOXCOORDINATE", "WOYCOORDINATE" = "WOYCOORDINATE")) %>% select(WORKORDERID, LOCATION = ADDRESS, FACILITYID) 
+     WOID_Based_XY<- WORKORDER_ID %>% select(workorder_id, wo_initiatedate, location, wo_xcoordinate, wo_ycoordinate) %>% inner_join(COORD_MATCHED_TABLE, by = c("wo_xcoordinate" = "wo_xcoordinate", "wo_ycoordinate" = "wo_ycoordinate")) %>% select(workorder_id, location = ADDRESS, facility_id = FACILITYID) 
      
   # union the matching tables-Final output is in FACID_ADD_XY
    
@@ -187,22 +187,22 @@
      
      WORKORDER_ID <- dbGetQuery(con, "SELECT * from fieldwork.wic_workorders")
      
-     WORK_DATE <- WORKORDER_ID %>% select(WORKORDERID, WO_INITIATEDATE)
+     WORK_DATE <- WORKORDER_ID %>% select(workorder_id, wo_initiatedate)
      
-     wic_parcels <- inner_join(wic_parcels, WORK_DATE, by = "WORKORDERID") %>% select(WORKORDERID, LOCATION, FACILITYID, WO_INITIATEDATE)
+     wic_parcels <- inner_join(wic_parcels, WORK_DATE, by = "workorder_id") %>% select(workorder_id, location, facility_id, wo_initiatedate)
     
    # Getting unique WIC Parcels
      
-     wic_facility_id <- wic_parcels %>% select(WORKORDERID, FACILITYID) %>%
-     group_by(FACILITYID) %>%
-     summarise(WORKORDERID = toString(sort(unique(WORKORDERID))))
+     wic_facility_id <- wic_parcels %>% select(workorder_id, facility_id) %>%
+     group_by(facility_id) %>%
+     summarise(workorder_id = toString(sort(unique(workorder_id))))
      
 ## Section 6: Writing results to DB
     
      con <- dbConnect(odbc(), dsn = "mars_data")
     
-     dbWriteTable (con, SQL("fieldwork.wic_parcels"),wic_parcels)
+     dbWriteTable (con, SQL("fieldwork.wic_parcels"),wic_parcels,append= TRUE, row.names = FALSE)
      
-     dbWriteTable (con, SQL("fieldwork.wic_facility_id"),wic_facility_id)
+     dbWriteTable (con, SQL("fieldwork.wic_facility_id"),wic_facility_id, append= TRUE, row.names = FALSE)
      
      dbDisconnect(GISDB)

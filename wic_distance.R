3a62a17b87b8aa894a2ece67a31d7873b685fbe7
Farshad Ebrahimi <Farshad.Ebrahimi@phila.gov>
2024-04-02 15:20:05 -0400

    calculating distance from the building footprints

--- wic_distance.R
+++ wic_distance.R
@@ -10,57 +10,49 @@
       library(tidyr)
       sf::sf_use_s2(FALSE)
       
-      con <- dbConnect(odbc::odbc(), dsn = "mars_data")
-      smp <- dbGetQuery(con, "SELECT * FROM fieldwork.wic_smp_mapid ")
-      parcel <- dbGetQuery(con, "SELECT * FROM fieldwork.wic_parcels_address")
+      con <- dbConnect(odbc::odbc(), dsn = "mars14_datav2", uid = Sys.getenv("shiny_uid"), pwd = Sys.getenv("shiny_pwd"), MaxLongVarcharSize = 8190  )
+      smp <- dbGetQuery(con, "SELECT * FROM fieldwork.tbl_wic_smp_wkt")
+      building_footprint <- dbGetQuery(con, "SELECT * FROM fieldwork.tbl_wic_buildingfootprint_wkt")
       
     # map geoprocessing
       
       smp_sf <- st_as_sfc(smp[,2], CRS = 4326)
-      parcel_sf <- st_as_sfc(parcel[,4], crs = 4326)
+      building_footprint_sf <- st_as_sfc(building_footprint[,5], crs = 4326)
       
-      smp_ids <- smp %>% select(SMP_ID)
+      smp_ids <- smp %>% select(smp_id)
       smp_spatial <- bind_cols(smp_ids, smp_sf)
       smp_spatial <- st_as_sf(smp_spatial)
       st_crs(smp_spatial) <- 4326
       
-      parcel_address <- parcel %>% select(-WKT)
-      parcel_spatial <- bind_cols(parcel_address, parcel_sf)
-      parcel_spatial <- st_as_sf(parcel_spatial)
-      st_crs(parcel_spatial) <- 4326
+      smp_bld_ftprint <- building_footprint %>% select(smp_id)
+      bld_spatial <- bind_cols(smp_bld_ftprint, building_footprint_sf)
+      bld_spatial <- st_as_sf(bld_spatial)
+      st_crs(bld_spatial) <- 4326
       
-      smp_spatial['system_id'] <- gsub('-\\d+$','',smp_spatial$SMP_ID) 
-      parcel_spatial['system_id'] <- gsub('-\\d+$','',parcel_spatial$smp_id ) 
-      parcel_address['system_id'] <- gsub('-\\d+$','',parcel_address$smp_id) 
-      
+      smp_spatial['system_id'] <- gsub('-\\d+$','',smp_spatial$smp_id) 
+      bld_spatial['system_id'] <- gsub('-\\d+$','',bld_spatial$smp_id ) 
+
       
       
 
   # distnace calculation
     
-    dist <- as.data.frame (matrix(NA, nrow = nrow(parcel_spatial), ncol = 1))
+    dist <- as.data.frame (matrix(NA, nrow = nrow(bld_spatial), ncol = 1))
     names(dist) <- "distance_ft"
     
-    for (i in 1:nrow(parcel_spatial)){
+    for (i in 1:nrow(bld_spatial)){
       
-      dist[i,1] <- st_distance(parcel_spatial[i,], smp_spatial %>%
-                                 filter(SMP_ID ==parcel_address[i,"smp_id"]))/0.3048
+      dist[i,1] <- st_distance(bld_spatial[i,], smp_spatial %>%
+                                 filter(smp_id ==smp_bld_ftprint[i,"smp_id"]))/0.3048
       
     }
-    parcel["dist"] <- dist
-    names(parcel) <- c("address","smp_id","buffer_ft","wkt","distance_ft")
-    parcel <- parcel[,c("smp_id","address","buffer_ft","distance_ft","wkt")]
-    
-    names(smp) <- c("smp_id","wkt")
     
     
-  # Write to table fieldwork.wic_parcels_address
+    building_footprint["distance_footprint_ft"] <- dist
     
-    dbWriteTable (con, SQL("fieldwork.wic_parcels_wkt"), parcel, append= TRUE, row.names = FALSE)
-    dbWriteTable (con, SQL("fieldwork.wic_smp_wkt"), smp, append= TRUE, row.names = FALSE)
+    building_footprint <- building_footprint %>%
+      select(-wic_buildingfootprint_wkt_uid, -distance_ft)
     
-    dbDisconnect(con)
-    
-    
+    dbWriteTable(con,Id(schema = "fieldwork", table = "tbl_wic_buildingfootprint_wkt"), building_footprint, append = TRUE )
     
     

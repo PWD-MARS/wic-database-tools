2d9c166f4ee8c02a934c95055e7d9292447f66c8
Farshad Ebrahimi <Farshad.Ebrahimi@phila.gov>
2024-07-23 16:35:30 -0400

    update

--- render.R
+++ render.R
@@ -8,7 +8,7 @@ report_name <- paste(datestring,".html", sep="")
 #Pandoc render script
 Sys.setenv(RSTUDIO_PANDOC = 'C:\\Program Files\\RStudio\\bin\\quarto\\bin\\pandoc')
 rmarkdown::render(
-  input = "C:\\Users\\Farshad.Ebrahimi\\OneDrive - City of Philadelphia\\Github Projects\\WIC\\update_wic.Rmd",
-  output_file = paste("C:\\Users\\Farshad.Ebrahimi\\OneDrive - City of Philadelphia\\Github Projects\\WIC\\Reports\\", report_name, sep = ""),
-  output_dir = "C:\\Users\\Farshad.Ebrahimi\\OneDrive - City of Philadelphia\\Github Projects\\WIC\\Reports\\"
+  input = "C:\\Users\\Farshad.Ebrahimi\\OneDrive - City of Philadelphia\\Github Projects\\wic-database-tools\\update_wic.Rmd",
+  output_file = paste("C:\\Users\\Farshad.Ebrahimi\\OneDrive - City of Philadelphia\\Github Projects\\wic-database-tools\\Reports\\", report_name, sep = ""),
+  output_dir = "C:\\Users\\Farshad.Ebrahimi\\OneDrive - City of Philadelphia\\Github Projects\\wic-database-tools\\Reports\\"
 )
